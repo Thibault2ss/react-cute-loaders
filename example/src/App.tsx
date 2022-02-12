ab944d3658e635862a96da2c3fab6366dbed93ce
Thibault de Saint Sernin <thibault@dfns.co>
2022-02-12 18:49:49 +0100

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-cute-loaders'
+import 'react-cute-loaders/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
